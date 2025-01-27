25e3811815bd28e389f6c2854b4990dea64ddf37
Roly Perera <roly.perera@dynamicaspects.org>
2025-01-27 08:12:16 +0000

    Moving average test.

--- website/Article/test.mjs
+++ website/Article/test.mjs
@@ -2,4 +2,5 @@ import { runTests, testURL, waitForFigure } from "./shared/webtest-lib.js";
 
 export const main = async () => {
    await runTests(testURL("convolution")([ page => waitForFigure(page)("fig-output") ]))();
+   await runTests(testURL("moving-average")([ page => waitForFigure(page)("fig-output") ]))();
 };
