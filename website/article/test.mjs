ff15e562bbf382361e09710fef7fbacd38d44d8a
Roly Perera <roly.perera@dynamicaspects.org>
2025-07-06 15:25:28 +0100

    Fixing up tests.

--- website/article/test.mjs
+++ website/article/test.mjs
@@ -1,10 +1,10 @@
-import { runTests, testURL, waitForFigure } from "./shared/webtest-lib.js";
+import { runTests, testURL, waitFor } from "./shared/webtest-lib.js";
 
 export const main = async () => {
-   await runTests(testURL("convolution")([ page => waitForFigure(page)("fig-output") ]))()
-   await runTests(testURL("moving-average")([ page => waitForFigure(page)("fig-output") ]))()
+   await runTests(testURL("convolution")([ page => waitFor("svg#fig-output")(page) ]))()
+   await runTests(testURL("moving-average")([ page => waitFor("svg#fig-output")(page) ]))()
    await runTests(testURL("non-renewables")([
-      page => waitForFigure(page)("fig-barChart"),
-      page => waitForFigure(page)("fig-scatterPlot"),
+      page => waitFor("svg#fig-barChart")(page),
+      page => waitFor("svg#fig-scatterPlot")(page),
    ]))()
 }
