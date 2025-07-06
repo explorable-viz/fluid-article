cc318a8612225bfce5e58b4b281717f6baa39dcc
Roly Perera <roly.perera@dynamicaspects.org>
2025-07-06 15:03:14 +0100

    Restore test.mjs.

---
+++ website/article/test.mjs
@@ -0,0 +1,10 @@
+import { runTests, testURL, waitForFigure } from "./shared/webtest-lib.js";
+
+export const main = async () => {
+   await runTests(testURL("convolution")([ page => waitForFigure(page)("fig-output") ]))()
+   await runTests(testURL("moving-average")([ page => waitForFigure(page)("fig-output") ]))()
+   await runTests(testURL("non-renewables")([
+      page => waitForFigure(page)("fig-barChart"),
+      page => waitForFigure(page)("fig-scatterPlot"),
+   ]))()
+}
