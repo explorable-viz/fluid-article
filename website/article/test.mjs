ec365db602d4488d8418a419a7cc65527bf6904a
Roly Perera <roly.perera@dynamicaspects.org>
2025-07-06 15:32:57 +0100

    Fix tests.

--- website/article/test.mjs
+++ website/article/test.mjs
@@ -1,10 +1,10 @@
-import { runTests, testURL, waitFor } from "./shared/webtest-lib.js";
+import { runTests, testURL, waitFor } from "./shared/webtest-lib.js"
 
 export const main = async () => {
    await runTests(testURL("convolution")([ page => waitFor("svg#fig-output")(page) ]))()
-   await runTests(testURL("moving-average")([ page => waitFor("svg#fig-output")(page) ]))()
+   await runTests(testURL("moving-average")([ page => waitFor("svg")(page) ]))()
    await runTests(testURL("non-renewables")([
-      page => waitFor("svg#fig-barChart")(page),
-      page => waitFor("svg#fig-scatterPlot")(page),
+      page => waitFor("#fig-output > svg:nth-child(1)")(page),
+      page => waitFor("#fig-output > svg:nth-child(2)")(page),
    ]))()
 }
